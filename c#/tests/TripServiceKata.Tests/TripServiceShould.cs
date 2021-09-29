00f8466340447da1cba88e76f44c906f35dbe744
José Antonio Fernández <jfernandez@aidacanarias.com>
2021-09-29 09:31:03 +0200

    add test throw_exception_when_user_is_not_logged_in (red)

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,20 @@
-﻿namespace TripServiceKata.Tests
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using TripServiceKata.Exception;
+
+namespace TripServiceKata.Tests
 {
+    [TestFixture]
     public class TripServiceShould
     {
+        [Test]
+        public void throw_exception_when_user_is_not_logged_in() {
+            var tripService = new TripService();
+
+            Action action = () => tripService.GetTripsByUser(null);
+
+            action.Should().Throw<UserNotLoggedInException>();
+        }
     }
 }
