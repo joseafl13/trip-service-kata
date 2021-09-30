8ea086b2fda57a375fc13af7a4b2fa48b3be344c
José Antonio Fernández <jfernandez@aidacanarias.com>
2021-09-30 08:29:02 +0200

    add test trip_list_is_empty_when_users_are_not_friends

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,20 +1,43 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using System;
+using TripServiceKata.Entity;
 using TripServiceKata.Exception;
 
 namespace TripServiceKata.Tests
 {
     [TestFixture]
-    public partial class TripServiceShould
+    public class TripServiceShould
     {
+        private static User loggedUser;
+
         [Test]
         public void throw_exception_when_user_is_not_logged_in() {
+            loggedUser = null;
             var tripService = new TripServiceMock();
 
             Action action = () => tripService.GetTripsByUser(null);
 
             action.Should().Throw<UserNotLoggedInException>();
+        }
+
+        [Test]
+        public void trip_list_is_empty_when_users_are_not_friends() {
+            loggedUser = new User();
+            var tripService = new TripServiceMock();
+
+            var aGivenUser = new User();
+            var friendTrips = tripService.GetTripsByUser(aGivenUser);
+
+            friendTrips.Should().BeEmpty();
+        }
+
+
+        private class TripServiceMock : TripService
+        {
+            protected override User GetLoggedUser() {
+                return loggedUser;
+            }
         }
     }
 }
